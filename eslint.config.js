22e57e1b1227cfd0124c4ca65687bda4456fd7c3
luizengdev <luizengdev@gmail.com>
2026-09-18 14:22:41 -0300

    chore: add eslint and prettier setup

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.nodeBuiltin,
+      },
+    },
+    rules: js.configs.recommended.rules,
+  },
+
+  ...tseslint.configs.recommended,
+
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+
+  eslintConfigPrettier,
+);
